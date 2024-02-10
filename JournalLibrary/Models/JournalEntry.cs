b72d8aa1cfa60d8e4caf306491ee7ec82145b165
Movsar Bekaev <movsar.dev@gmail.com>
2024-02-10 12:08:19 +0300

    save

---
+++ JournalLibrary/Models/JournalEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalLibrary.Models
+{
+    public class JournalEntry
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime Date { get; set; }
+        public decimal Temperature { get; set; }
+        public decimal Humidity { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- WeatherJournalWPF/MainWindow.xaml.cs
+++ WeatherJournalWPF/MainWindow.xaml.cs
@@ -1,43 +1,26 @@
-﻿using System.IO;
-using System.Text;
+﻿
+using JournalLibrary;
+using JournalLibrary.Models;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+
 
 namespace WeatherJournalWPF
 {
   
     public partial class MainWindow : Window
     {
-        public const string PATH = @"Weather.txt";
-        readWindow readWindow = new readWindow();
-        writeWindow writeWindow = new writeWindow();
+        public ObservableCollection<JournalEntry> JournalEntries { get; } = new ObservableCollection<JournalEntry>();
         public MainWindow()
         {
             InitializeComponent();
-
-            RadioButton choiceRead = rdbDataRead;
-            RadioButton choiceWrite = rdbDataWrite;
-            choiceRead.Checked += RadioButton_Checked;
-            choiceWrite.Checked += RadioButton_Checked;
-        }
-
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            if (rdbDataRead.IsChecked == true)
+            JournalStorage storage = new JournalStorage();
+            foreach(var entry in storage.GetAllJournalEntrys())
             {
-                readWindow.ShowDialog();
+                JournalEntries.Add(entry);
             }
-            else if (rdbDataWrite.IsChecked == true)
-            {
-                writeWindow.ShowDialog();
-            }
+            
         }
 
     }
