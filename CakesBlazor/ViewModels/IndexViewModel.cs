046bc47436b53a3cea48bcd1f3c84961a1147d84
FraintSaad <islamqwertyu@mail.ru>
2024-04-26 20:36:12 +0300

    commit with cakesBlazor

--- CakesBlazor/ViewModels/IndexViewModel.cs
+++ CakesBlazor/ViewModels/IndexViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace CakesBlazor.ViewModels
+﻿using CakesLibrary.Models;
+using System.Collections.ObjectModel;
+
+namespace CakesBlazor.ViewModels
 {
     public class IndexViewModel
     {
+        public ObservableCollection<Ingredient> Ingredients { get; set; } = new ObservableCollection<Ingredient>();
+        public ObservableCollection<string> Recipes { get; set; } = new ObservableCollection<string>();
+        private Storage _storage;
+        private Kitchen _kitchen;
     }
 }
